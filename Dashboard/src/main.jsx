df711bb0eb588e7cf33044a5ba46f252a55a0dd4
kuruva-radhakrishna <kuruvaradhakrishna77@gmail.com>
2024-10-13 16:05:07 +0530

    updateUser  context

--- Dashboard/src/main.jsx
+++ Dashboard/src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import { UserContextProvider } from "./components/UserContext";
+import userContext, { UserContextProvider } from "./components/UserContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const {user,updateUser} = useContext(userContext);
+console.log(user);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
