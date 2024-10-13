2ab9c926f00022bd85f2e2fd2749f7d9cd7f9062
kuruva-radhakrishna <kuruvaradhakrishna77@gmail.com>
2024-10-13 14:48:58 +0530

    editing dashboard to deploy

--- Dashboard/vite.config.js
+++ Dashboard/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000,  // Use the dynamic port from Render, fallback to 3000 if not set
+    host: '0.0.0.0',  // Bind to all available network interfaces
+  }
 })
